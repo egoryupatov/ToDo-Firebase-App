93232e591dd13308c5c9f82d10215c61a0cf8de8
Egor Yupatov <95219188+egoryupatov@users.noreply.github.com>
2022-11-20 16:56:09 +0600

    first commit

---
+++ src/components/TasksList/TasksList.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { TasksListStyled } from "./TasksList/TasksList.styled";
+
+export const TasksList: React.FC = () => {
+  return (
+    <TasksListStyled>
+      <h1>To-Do List</h1>
+    </TasksListStyled>
+  );
+};
